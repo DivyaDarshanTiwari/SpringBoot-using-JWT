551a81025a6849a20da984a6c5672104968f6bd5
DivyaDarshanTiwari <davidrocco400@outlook.com>
2025-04-13 21:40:09 +0530

    all the depenedencies need for the project

---
+++ src/main/java/com/example/springoot_using_jwt/SpringootUsingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springoot_using_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringootUsingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringootUsingJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springoot_using_jwt/SpringootUsingJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoot_using_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringootUsingJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
